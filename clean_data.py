289954948836eaee5c5e44ff7516072fa598e964
William Philipp <william_philipp@web.de>
2022-04-15 10:23:10 +0200

    cleaned cat town data

---
+++ clean_data.py
@@ -0,0 +1,21 @@
+#clean the "Venim de" and "Venim d'" from the Cat towns
+
+
+def clean_data(file_path, output_path):
+    # Read in the file
+    with open(file_path, 'r') as file :
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("Venim de ", '')
+    filedata = filedata.replace("Venim del ", '')
+    filedata = filedata.replace("Venim dels ", '')
+    filedata = filedata.replace("Venim d'", '')
+    filedata = filedata.replace(".", '')
+    # Write the file out again
+    with open(output_path, 'w') as file:
+        file.write(filedata)
+
+clean_data("data\muni-bal.txt", "data\muni-bal-cleaned.txt")
+clean_data("data\muni-cat.txt", "data\muni-cat-cleaned.txt")
+clean_data("data\muni-val.txt", "data\muni-val-cleaned.txt")
