547b7251eb2cc38e9b80d3bf0f6a17119f659466
William Philipp <william_philipp@web.de>
2022-04-15 10:38:17 +0200

    generated csv for cat towns

---
+++ txt_to_csv.py
@@ -0,0 +1,11 @@
+#convert the txt cat town names to csv
+import pandas as pd
+
+read_file = pd.read_csv (r'data\raw\muni-val-cleaned.txt')
+read_file.to_csv (r'data\cat.csv', index=None)
+
+read_file = pd.read_csv (r'data\raw\muni-cat-cleaned.txt')
+read_file.to_csv (r'data\bal.csv', index=None)
+
+read_file = pd.read_csv (r'data\raw\muni-bal-cleaned.txt')
+read_file.to_csv (r'data\val.csv', index=None)
